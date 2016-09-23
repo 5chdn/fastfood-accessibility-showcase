8ec656ee51496ee28fe3eeb2d3416e8213bd037e
5chdn <schoedon@mailbox.org>
2016-09-23 15:43:04 +0200

    Revert debugging changes.

--- accessibility.js
+++ accessibility.js
@@ -496,10 +496,8 @@ function drawGL() {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texImage2D(
       gl.TEXTURE_2D,
       0,
@@ -531,7 +529,7 @@ function drawGL() {
     gl.uniformMatrix4fv(sp.uniformMatrix, false, uMatrix);
 
     /* adjust line width based on zoom */
-    gl.lineWidth(10);
+    gl.lineWidth(width);
 
     /* loop all tile buffers in cache and draw each geometry */
     let tileBuffers = TILE_CACHE.getTileBufferCollection();
